d6878603ad95cfdee3a2e471b36504667c5cd776
izzy-pin <66907465+izzy-pin@users.noreply.github.com>
2022-01-10 16:14:34 +0000

    add isLoading and isError state to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,12 +4,23 @@ import { getTopics } from "../utils/api";
 
 const Nav = () => {
   const [topics, setTopics] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    getTopics().then((topicsFromApi) => {
-      setTopics(topicsFromApi);
-    });
+    setIsError(false);
+    setIsLoading(true);
+    getTopics()
+      .then((topicsFromApi) => {
+        setTopics(topicsFromApi);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsError(true);
+        setIsLoading(false);
+      });
   }, []);
+
   return (
     <nav className="Nav">
       <NavLink
@@ -21,22 +32,29 @@ const Nav = () => {
         {" "}
         news rn
       </NavLink>
+
       <div className="Nav--TopicsDiv">
-        {topics.map((topic) => {
-          return (
-            <NavLink
-              className={({ isActive }) =>
-                isActive
-                  ? "NavLink NavLink--Topics NavLink--Active"
-                  : "NavLink--Topics NavLink"
-              }
-              to={`/topics/${topic.slug}`}
-              key={topic.slug}
-            >
-              {topic.slug}
-            </NavLink>
-          );
-        })}
+        {isLoading ? (
+          <span>preparing topics...</span>
+        ) : isError ? (
+          <span>sorry, an error has occured</span>
+        ) : (
+          topics.map((topic) => {
+            return (
+              <NavLink
+                className={({ isActive }) =>
+                  isActive
+                    ? "NavLink NavLink--Topics NavLink--Active"
+                    : "NavLink--Topics NavLink"
+                }
+                to={`/topics/${topic.slug}`}
+                key={topic.slug}
+              >
+                {topic.slug}
+              </NavLink>
+            );
+          })
+        )}
       </div>
     </nav>
   );
