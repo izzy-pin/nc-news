0476ca3094a456c3501dd0eb33622c577afcbb7d
izzy-pin <66907465+izzy-pin@users.noreply.github.com>
2022-01-13 17:47:33 +0000

    abstract SortBy dropdowns into own component

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -2,14 +2,16 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getArticles } from "../utils/api";
 import ArticleCard from "./ArticleCard";
+import ArticlesPag from "./ArticlesPag";
+import SortBy from "./SortBy";
 
 const ArticlesList = () => {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
-  const [sortByValue, setSortByValue] = useState("created_at");
+  const [error, setError] = useState({ status: undefined, msg: "error" });
+
   const [sort_by, setSort_by] = useState(undefined);
-  const [orderValue, setOrderValue] = useState("desc");
   const [order, setOrder] = useState(undefined);
 
   //destructure topic from the params obj
@@ -26,73 +28,33 @@ const ArticlesList = () => {
       .catch((err) => {
         setIsLoading(false);
         setIsError(true);
-        console.log(err.response.data.msg);
-        console.log(err.response.status);
+        setError({ status: err.response.status, msg: err.response.data.msg });
       });
   }, [topic, sort_by, order]);
-
-  const handleSortByChange = (event) => {
-    setSortByValue(event.target.value);
-  };
-
-  const handleOrderChange = (event) => {
-    setOrderValue(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setSort_by(sortByValue);
-    setOrder(orderValue);
-  };
 
   return (
     <main>
       <h1>{topic ? topic : "all topics"} </h1>
-      <section className="SortBy__Section">
-        <h2>Sort by:</h2>
-        <form onSubmit={handleSubmit}>
-          <select
-            className="ArticleListSort__select"
-            defaultValue={sortByValue}
-            onChange={handleSortByChange}
-          >
-            {" "}
-            <option value="created_at">Date posted</option>
-            <option value="comment_count">Comment count</option>
-            <option value="votes">Likes</option>
-          </select>
-          <select
-            className="ArticleListOrder__select"
-            defaultValue={orderValue}
-            onChange={handleOrderChange}
-          >
-            {" "}
-            <option value="desc">
-              {sortByValue === "created_at"
-                ? "newest to oldest"
-                : "most to least popular"}
-            </option>
-            <option value="asc">
-              {sortByValue === "created_at"
-                ? "oldest to newest"
-                : "least to most popular"}
-            </option>
-          </select>
-
-          <button className="ArticleListSort__button">Go</button>
-        </form>
-      </section>
 
       {isLoading ? (
         <p>Loading...</p>
       ) : isError ? (
-        <p>Sorry, there was an error</p>
+        <section className="ErrorSection">
+          <p>Sorry :( </p>
+          <p>
+            {error.status}, {error.msg}
+          </p>
+        </section>
       ) : (
-        <ul className="ArticleList">
-          {articles.map((article) => (
-            <ArticleCard key={article.article_id} article={article} />
-          ))}
-        </ul>
+        <>
+          <SortBy setSort_by={setSort_by} setOrder={setOrder} />
+          <ul className="ArticleList">
+            {articles.map((article) => (
+              <ArticleCard key={article.article_id} article={article} />
+            ))}
+          </ul>
+          <ArticlesPag />
+        </>
       )}
     </main>
   );
---
+++ src/components/SortBy.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+const SortBy = ({ setSort_by, setOrder }) => {
+  const [sortByValue, setSortByValue] = useState("created_at");
+  const [orderValue, setOrderValue] = useState("desc");
+
+  const handleSortByChange = (event) => {
+    setSortByValue(event.target.value);
+  };
+
+  const handleOrderChange = (event) => {
+    setOrderValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSort_by(sortByValue);
+    setOrder(orderValue);
+  };
+  return (
+    <section className="SortBy__Section">
+      <h2>Sort by:</h2>
+      <form onSubmit={handleSubmit}>
+        <select
+          className="ArticleListSort__select"
+          defaultValue={sortByValue}
+          onChange={handleSortByChange}
+        >
+          {" "}
+          <option value="created_at">Date posted</option>
+          <option value="comment_count">Comment count</option>
+          <option value="votes">Likes</option>
+        </select>
+        <select
+          className="ArticleListOrder__select"
+          defaultValue={orderValue}
+          onChange={handleOrderChange}
+        >
+          {" "}
+          <option value="desc">
+            {sortByValue === "created_at"
+              ? "newest to oldest"
+              : "highest to lowest"}
+          </option>
+          <option value="asc">
+            {sortByValue === "created_at"
+              ? "oldest to newest"
+              : "lowest to highest"}
+          </option>
+        </select>
+
+        <button className="ArticleListSort__button">Go</button>
+      </form>
+    </section>
+  );
+};
+
+export default SortBy;
