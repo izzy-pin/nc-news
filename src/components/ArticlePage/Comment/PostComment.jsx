00ad5aaa2d7355917af8f34575403d4263e06b5e
izzy-pin <66907465+izzy-pin@users.noreply.github.com>
2022-01-13 15:56:15 +0000

    add error handling

--- src/components/ArticlePage/Comment/PostComment.jsx
+++ src/components/ArticlePage/Comment/PostComment.jsx
@@ -5,6 +5,8 @@ import { useImageUrl } from "../../../utils/useImageUrl";
 const PostComment = ({ article_id, setComments }) => {
   const [textAreaInput, setTextAreaInput] = useState("");
   const [data, setData] = useState({ username: "tickle122", body: "" });
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const imageUrl = useImageUrl(data.username);
 
@@ -17,15 +19,20 @@ const PostComment = ({ article_id, setComments }) => {
   };
 
   const handleSubmit = (event) => {
+    setIsLoading(true);
+    setIsError(false);
     event.preventDefault();
     setTextAreaInput("");
     postComment(article_id, data)
       .then((commentFromApi) => {
+        setIsLoading(false);
         setComments((currComments) => {
           return [commentFromApi, ...currComments];
         });
       })
       .catch((err) => {
+        setIsLoading(false);
+        setIsError(true);
         console.log(err.response.data.msg);
         console.log(err.response.status);
       });
@@ -38,7 +45,7 @@ const PostComment = ({ article_id, setComments }) => {
         src={imageUrl}
         alt="user's profile pic"
       ></img>
-      <form onSubmit={handleSubmit}>
+      <form className="PostComment__Form" onSubmit={handleSubmit}>
         <textarea
           required
           value={textAreaInput}
@@ -46,7 +53,16 @@ const PostComment = ({ article_id, setComments }) => {
           placeholder="Thoughts?..."
           className="PostComment__textarea"
         ></textarea>
-        <button className="PostComment__buttom">Comment</button>
+        <button className="PostComment__button">Comment</button>
+
+        <span>
+          {" "}
+          {isLoading
+            ? "Posting your comment..."
+            : isError
+            ? "Sorry, there was an error, please try again"
+            : null}
+        </span>
       </form>
     </section>
   );
