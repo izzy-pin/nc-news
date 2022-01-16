6abda35646fa2d3029edaf081d09928be45396ed
izzy-pin <66907465+izzy-pin@users.noreply.github.com>
2022-01-16 11:49:03 +0000

    refactor singleArticle into singleArticlePage component

--- src/components/ArticlePage/SingleArticlePage.jsx
+++ src/components/ArticlePage/SingleArticlePage.jsx
@@ -1,14 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { useState } from "react/cjs/react.development";
 import { getArticleByArticleId } from "../../utils/api";
-import SingleArticle from "./SingleArticle";
+import CommentList from "./Comment/CommentList";
+import ArticleVotes from "./ArticleVotes";
+import PostComment from "./Comment/PostComment";
 
 const SingleArticlePage = () => {
   const [singleArticle, setSingleArticle] = useState({});
   const [isError, setIsError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState({ status: undefined, msg: "error" });
+  const createdDate = new Date(singleArticle.created_at);
+  const [comments, setComments] = useState([]);
 
   const { article_id } = useParams();
 
@@ -28,10 +31,9 @@ const SingleArticlePage = () => {
   }, [article_id]);
 
   return (
-    <div>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : isError ? (
+    <section className="SingleArticlePage__Section">
+      {isLoading ? <p>Loading...</p> : null}
+      {isError ? (
         <section className="ErrorSection">
           <p>Sorry, there was an error :( </p>
           <p>
@@ -39,9 +41,28 @@ const SingleArticlePage = () => {
           </p>
         </section>
       ) : (
-        <SingleArticle article={singleArticle} />
+        <main className="SingleArticle__main">
+          <article className="SingleArticle__article">
+            <p className="SingleArticle__topic">{singleArticle.topic}</p>
+            <h2 className="SingleArticle__title">{singleArticle.title}</h2>
+            <p className="SingleArticle__author">{singleArticle.author}</p>{" "}
+            <p className="SingleArticle__date">{createdDate.toUTCString()}</p>
+            <p className="SingleArticle__body">{singleArticle.body}</p>
+            <div className="CommentVote__Div">
+              <span>Comments: {singleArticle.comment_count}</span>{" "}
+              <ArticleVotes article={singleArticle} />
+            </div>
+          </article>
+
+          <PostComment
+            article_id={singleArticle.article_id}
+            setComments={setComments}
+          />
+
+          <CommentList comments={comments} setComments={setComments} />
+        </main>
       )}
-    </div>
+    </section>
   );
 };
 
