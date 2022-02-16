be7a939fb9f6a686aea2a4cb709999a97a3b99c7
izzy-pin <66907465+izzy-pin@users.noreply.github.com>
2022-02-16 18:52:28 +0000

    update footer copy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,46 @@
 const Footer = () => {
   return (
     <footer>
-      {" "}
-      news rn back-end on{" "}
-      <a
-        href="https://github.com/izzy-pin/news-rn"
-        target="_blank"
-        rel="noreferrer"
-      >
-        GitHub <i className="fab fa-github"></i>
-      </a>{" "}
-      and{" "}
-      <a
-        href="https://newsrn.herokuapp.com/api/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        {" "}
-        Heroku <i className="fas fa-server"></i>
-      </a>
+      <section>
+        <p>
+          By
+          <a
+            href="https://github.com/izzy-pin"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Izzy Pinder
+          </a>
+        </p>
+        <p>
+          Find this project on
+          <a
+            href="https://github.com/izzy-pin/news-rn"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub <i className="fab fa-github"></i>
+          </a>
+        </p>
+      </section>
+      <p>
+        View news rn back-end on
+        <a
+          href="https://github.com/izzy-pin/news-rn-back-end"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub <i className="fab fa-github"></i>
+        </a>
+        and
+        <a
+          href="https://newsrn.herokuapp.com/api/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Heroku <i className="fas fa-server"></i>
+        </a>
+      </p>
     </footer>
   );
 };
