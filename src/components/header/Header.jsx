8dc5d6049b283f2873fd04ce5b21aa7439a41f5a
izzy-pin <66907465+izzy-pin@users.noreply.github.com>
2022-03-08 16:04:04 +0000

    create icon in header to link to PostArticle component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { DefaultUserContext } from "../../contexts/DefaultUser";
+
 import Nav from "./Nav";
 
 const Header = () => {
   const currentDate = new Date();
+  const { user } = useContext(DefaultUserContext);
 
   const options = {
     weekday: "long",
@@ -13,8 +18,15 @@ const Header = () => {
   return (
     <header>
       <Nav />
-      <section className="Section__Date">
-        {currentDate.toLocaleDateString("en-uk", options)}
+      <section className="DatePost__Section">
+        <p className="Date__p">
+          {currentDate.toLocaleDateString("en-uk", options)}
+        </p>
+        {user.username === "tickle122" ? (
+          <Link className="postArticle__link" to="post-article">
+            <i className="fas fa-edit"></i>
+          </Link>
+        ) : null}
       </section>
     </header>
   );
